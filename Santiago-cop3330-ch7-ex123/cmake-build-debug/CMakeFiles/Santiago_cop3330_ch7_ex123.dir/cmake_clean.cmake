626946d983e2576cc8042b1cb8ea6c05c1e6af97
paradoxAlpha823 <55543878+paradoxAlpha823@users.noreply.github.com>
2021-12-07 00:35:18 -0500

    Debug and added capabilities to buggy calculator submission.

---
+++ Santiago-cop3330-ch7-ex123/cmake-build-debug/CMakeFiles/Santiago_cop3330_ch7_ex123.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Santiago_cop3330_ch7_ex123.dir/main.cpp.obj"
+  "Santiago_cop3330_ch7_ex123.exe"
+  "Santiago_cop3330_ch7_ex123.exe.manifest"
+  "Santiago_cop3330_ch7_ex123.lib"
+  "Santiago_cop3330_ch7_ex123.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Santiago_cop3330_ch7_ex123.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
